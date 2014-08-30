15a144ffef57013e64a372d899a36ef9fbf24cec
Brian Hiatt <brian.hiatt41@gmail.com>
2014-08-30 15:44:09 -0500

    user show map & message count

---
+++ app/static/js/user/clientShow.js
@@ -0,0 +1,23 @@
+/* global initMap, addMarker */
+
+(function(){
+  'use strict';
+
+  $(document).ready(function(){
+    var pos = getPosition();
+    var map = initMap('smallmap', pos.lat, pos.lng, 10);
+    addMarker(map, pos.lat, pos.lng, pos.name);
+  });
+
+  function getPosition(){
+    var $location = $('#location'),
+        name      = $location.attr('data-location'),
+        lat       = $location.attr('data-lat'),
+        lng       = $location.attr('data-lng'),
+        pos       = {name:name, lat:parseFloat(lat), lng:parseFloat(lng)};
+
+    return(pos);
+  }
+
+})();
+
